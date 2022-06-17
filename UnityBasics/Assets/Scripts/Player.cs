5aec244b60ada874cfb29ec039418b75aa2202d8
tianachan <107435880+tianachan@users.noreply.github.com>
2022-06-16 18:48:16 -0700

    Add project

---
+++ UnityBasics/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if space key is pressed down
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("beep boop tax evasion");
+            GetComponent<Rigidbody>;
+        }
+    }
+}
